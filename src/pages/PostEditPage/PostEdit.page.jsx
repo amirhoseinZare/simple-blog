149603518c6d5bdc7157519785f219d1588bf131
amir <zareamirhussein@gmail.com>
2021-07-27 16:02:40 +0430

    finishing ui of post-edit-page

--- src/pages/PostEditPage/PostEdit.page.jsx
+++ src/pages/PostEditPage/PostEdit.page.jsx
@@ -1,28 +1,39 @@
-import {InputForm} from "../../components/index"
 import {useState, useEffect} from "react"
 import {postApi} from "../../api/index"
+import { useParams } from "react-router"
+import TextField from '@material-ui/core/TextField';
+import classes from "./PostEdit.module.scss"
 
 export const PostEditPage = ()=>{
     const [formState, setFormState] = useState({ title:'', body:'' })
+    const {postId} = useParams()
 
-    const inputHandler = ({target:value}, key)=>{
+    const inputHandler = ({target:{value}}, key)=>{
         setFormState({ ...formState, [key]:value })
     }
 
     const getPost = async ()=>{
-        const {data:post} = postApi.get('1')
-        console.log(post)
+        console.log(postId)
+        const {data:{body, title}} = await postApi.get(postId)
+        console.log(body,'-----', title)
+        setFormState({body:body, title:title})
     }
 
     useEffect(()=>{
         getPost()
     }, [])
 
-    const { title, description } = formState
+    const { title, body } = formState
     return (
         <main>
-            <InputForm value={title} handleChange={(e)=>inputHandler(e,'title')} label='title'/>
-            <InputForm value={description} handleChange={(e)=>inputHandler(e,'body')} label='body'/>
+            <div className={classes.inputsContainer}>
+                <TextField className={classes.inputItem} id="outlined-basic" label="title" variant="outlined" value={title} onChange={(e)=>inputHandler(e, 'title')}/>
+                <TextField className={classes.inputItem} id="outlined-basic" label="body" variant="outlined" value={body} onChange={(e)=>inputHandler(e,'body')}/>
+                <div className={classes.buttonsContainer}>
+                    <button className={classes.buttonItem}>save</button>
+                    <button className={classes.buttonItem}>cancel</button>
+                </div>
+            </div>
         </main>
     )
 }
--- src/router/App.route.jsx
+++ src/router/App.route.jsx
@@ -1,8 +1,9 @@
 import { Switch, Route } from "react-router-dom"
-import { HomePage} from "../pages/index"
+import { HomePage, PostEditPage } from "../pages/index"
 
 export const AppRouter = () =>(
     <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/post/edit/:postId" component={PostEditPage} />
     </Switch>
 )
