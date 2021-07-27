0c7e4032ec7e136508a8da93bfba940c17421053
amir <zareamirhussein@gmail.com>
2021-07-28 01:32:14 +0430

    adding dynamic changinc of color to UserList and UserPosts component

--- src/components/UserList/UserList.component.jsx
+++ src/components/UserList/UserList.component.jsx
@@ -1,10 +1,16 @@
 import classes from "./UserList.module.scss"
 import {UserCard} from "../index"
+import {useSelector} from "react-redux"
 
-export const UserList = ({users}) => (
-    <section className={classes.userList}>
-        {
-            users.map((user)=> <UserCard key={user.id} user ={user}/>)
-        }
-    </section>
-)
+export const UserList = ({users}) => {
+
+    const mode = useSelector(({theme:{mode}})=>mode)
+
+    return (
+        <section className={classes.userList} style={{backgroundColor:mode==='light'?'#C4C4C4':'#000000'}}>
+            {
+                users.map((user)=> <UserCard key={user.id} user ={user}/>)
+            }
+        </section>
+    )
+}
--- src/components/UserPosts/UserPosts.component.jsx
+++ src/components/UserPosts/UserPosts.component.jsx
@@ -1,9 +1,12 @@
 import classes from "./UserPosts.module.scss"
 import {PostCard} from "../index"
+import {useSelector} from "react-redux"
 
 export const UserPosts = ({posts, user}) => {
+    const mode = useSelector(({theme:{mode}})=>mode)
+
     return (
-        <section className={classes.postList}>
+        <section className={classes.postList} style={{backgroundColor:mode==='light'?'#C4C4C4':'#000000'}}>
             <h2 className={classes.postsHeader}>User {user} posts</h2>
             {
                 posts.map(({id, title, body})=> {
